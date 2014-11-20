2c86b125399c1212edcee8c588138a39a4ae8280
Todd Wolfson <todd@twolfson.com>
2014-11-20 04:04:22 -0800

    Added test for private case

--- test/grunt-init-node-test.js
+++ test/grunt-init-node-test.js
@@ -79,3 +79,54 @@ describe('An UNLICENSE init', function () {
     finder.on('end', done);
   });
 });
+
+describe('A private init', function () {
+  before(function (done) {
+    // Relocate to the test directory
+    var actualDir = __dirname + '/actual_files/private';
+    rimraf.sync(actualDir);
+    mkdirp.sync(actualDir);
+    process.chdir(actualDir);
+
+    // Save test dirs for later
+    this.actualDir = actualDir;
+    this.expectedDir = __dirname + '/expected_files/private';
+
+    // Run the grunt-init script inside of the test directory
+    // TODO: Consider how to convert this to a flat file
+    suppose('grunt-init', ['node'])
+      .debug(process.stdout)
+      .on(/Project name/).respond('test-private\n')
+      .on(/Description/).respond('Private test project\n')
+      .on(/Version/).respond('9.0.0\n')
+      .on(/Project git repository/).respond('git://abc\n')
+      .on(/Project homepage/).respond('http://abc\n')
+      .on(/Project issues tracker/).respond('http://abc/issues\n')
+      .on(/Licenses/).respond('UNLICENSE\n')
+      .on(/Author name/).respond('Todd Wolfson\n')
+      .on(/Author email/).respond('todd@twolfson.com\n')
+      .on(/Author url/).respond('http://twolfson.com/\n')
+      .on(/Travis CI username/).respond('twolfson\n')
+      .on(/Gratipay username/).respond('twolfson\n')
+      .on(/What versions of node/).respond('>= 0.8.0\n')
+      .on(/Main module\/entry point/).respond('lib/test-private\n')
+      .on(/Npm test command/).respond('mocha\n')
+      .on(/What keywords/).respond('a, b, c\n')
+      .on(/any changes/).respond('n\n')
+      ['error'](done)
+      .end(function (code) {
+        assert.strictEqual(code, 0, 'Exited with non-zero code (' + code + ')');
+        done();
+      });
+  });
+
+  it('adds a private flag to the `package.json`', function (done) {
+    var filepath = __dirname + '/actual_files/private/package.json',
+        actualFile = fs.readFileSync(filepath, 'utf8'),
+        expectedFilepath = filepath.replace('/actual_files/', '/expected_files/'),
+        expectedFile = fs.readFileSync(expectedFilepath, 'utf8');
+
+    // Assert their content is equal
+    assert.strictEqual(actualFile, expectedFile, 'Content of "' + filepath + '" did not match as expected');
+  });
+});
